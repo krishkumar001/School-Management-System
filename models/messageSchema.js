b8661f86ecff1dcb27ab4e15283c807e340a16c4
krishkumar001 <kumarkrish1928@gmail.com>
2025-07-08 13:10:04 +0530

    Initial backend commit

---
+++ models/messageSchema.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const messageSchema = new mongoose.Schema({
+    sender: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        refPath: 'senderModel'
+    },
+    senderModel: {
+        type: String,
+        required: true,
+        enum: ['admin', 'teacher', 'student', 'parent']
+    },
+    receiver: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        refPath: 'receiverModel'
+    },
+    receiverModel: {
+        type: String,
+        required: true,
+        enum: ['admin', 'teacher', 'student', 'parent']
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    timestamp: {
+        type: Date,
+        default: Date.now
+    },
+    read: {
+        type: Boolean,
+        default: false
+    },
+    conversationId: {
+        type: String
+    }
+});
+
+module.exports = mongoose.model('message', messageSchema); 
